06a755623a217ee5bbe28b033edf697a35a570db
niranjansd <niranjan.sd@gmail.com>
2023-05-04 00:04:55 -0700

    update bio

---
+++ __init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from . import *
+
+
+# Autorship information
+__author__ = "Niranjan Sridhar"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Niranjan Sridhar"]
+__license__ = "GNU General Public License v3.0"
+__maintainer__ = "Niranjan Sridhar"
+__email__ = "niranjan.sd@gmail.com"
